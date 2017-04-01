d43ddbbeee6c42dd6918d5a10407f7c956ba7f9c
Bharath V <Bharath V>
2017-04-02 01:21:33 +0530

    Partial completion of Step1

--- testB/AsgSearch.web/Controllers/HomeController.cs
+++ testB/AsgSearch.web/Controllers/HomeController.cs
@@ -1,4 +1,15 @@
-﻿using System.Web.Mvc;
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using System.Net;
+using System.IO;
+using System.Collections.Generic;
+using System;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Net.Http.Headers;
+using AsgSearch.web.Classes;
 
 namespace AsgSearch.web.Controllers
 {
@@ -27,6 +38,39 @@ namespace AsgSearch.web.Controllers
             ViewBag.Title = "Step 3";
 
             return View();
+        }
+
+        public async Task<ActionResult> Search(string searchTerm)
+        {
+            //TODO: Add Summary,comments, log and handle service call in other layers to improve test ability.
+            var response = String.Empty;
+            RootObject ro = null;
+            try
+            {
+                var apiUrl = "http://api.stackexchange.com/2.2/search/advanced?pagesize=40&q={0}&accepted=True&body={1}&tagged={2}&site=stackoverflow";
+                apiUrl = String.Format(apiUrl, searchTerm, searchTerm, searchTerm);
+
+                HttpClientHandler handler = new HttpClientHandler();
+                handler.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+                using (var httpClient = new HttpClient(handler))
+                {
+                    httpClient.BaseAddress = new Uri(apiUrl);
+                    httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    response = await httpClient.GetStringAsync(apiUrl);
+
+                    JavaScriptSerializer json_serializer = new JavaScriptSerializer();
+                    ro = json_serializer.Deserialize<RootObject>(response);
+                    ViewBag.items = ro.items;
+                }
+            }catch(Exception e)
+            {
+                //TODO: Handle error
+                return Content("Failure");
+            }
+            
+            //TODO: Return better response
+            return PartialView("Step1", ro.items);
         }
     }
 }
